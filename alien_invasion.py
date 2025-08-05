510a042da0002995d759ae27f3f0b0e637d1eed5
Thiago Fernandes <devcyber.thiago@gmail.com>
2025-08-05 09:36:06 -0300

    Criação do arquivo e classe Settings, para concentrar todas as configurações do jogo

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,5 +1,6 @@
 import sys
 import pygame
+from settings import Settings
 
 class AlienInvasion:
     """
@@ -10,11 +11,10 @@ class AlienInvasion:
         """Inicializa o jogo e cria seus recursos necessários."""
         pygame.init()
         self.clock = pygame.time.Clock()
+        self.settings = Settings()
 
-        self.screen = pygame.display.set_mode((800,400))
+        self.screen = pygame.display.set_mode((self.settings.screen_width,self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
-
-        self.bg_color = (230,230,230)
 
     def run_game(self):
         """Inicia o loop principal do jogo."""
@@ -23,8 +23,8 @@ class AlienInvasion:
                 if event.type == pygame.QUIT:
                     sys.exit()
 
-            self.screen.fill(self.bg_color)
-            
+            self.screen.fill(self.settings.bg_color)
+
             # torna o desenho de tela mais recente visível
             pygame.display.flip()
             self.clock.tick(60)
---
+++ settings.py
@@ -0,0 +1,14 @@
+class Settings:
+    """
+    Classe para armazenar todos os valores de configurações do jogo.
+    """
+
+    def __init__(self):
+        """Inicializa as configurações de uma instância do jogo."""
+
+        # Configurações de tal
+        self.screen_width = 800
+        self.screen_height = 400
+        self.bg_color = (230,230,230)
+
+        
