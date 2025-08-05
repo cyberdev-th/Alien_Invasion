96d4bb759542d03fc18716d2ff34fe8cf82efbb5
Thiago Fernandes <devcyber.thiago@gmail.com>
2025-08-05 10:42:12 -0300

    Criação do arquivo e classe ship, que controla a nave

---
+++ ship.py
@@ -0,0 +1,20 @@
+import pygame
+
+class Ship:
+    """Classe que gerencia a nave."""
+
+    def __init__(self, ai_game):
+        """Inicializa a nave and configura sua posição inicial."""
+        self.screen = ai_game.screen
+        self.screen_rect = ai_game.screen.get_rect()
+
+        # carregando a imagem e obtendo seu retângulo
+        self.image = pygame.image.load('images/ship.bmp')
+        self.rect = self.image.get_rect()
+
+        # posicionando a nave ao centro inferior da tela
+        self.rect.midbottom = self.screen_rect.midbottom
+
+    def blitme(self):
+        """Desenha a nave an sua posição atual."""
+        self.screen.blit(self.image, self.rect)
