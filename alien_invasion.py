797d83e3d8a5e0b96edfeafc09c6715497116819
Thiago Fernandes <devcyber.thiago@gmail.com>
2025-08-05 09:11:32 -0300

    Criação da tela do jogo e do arquivo run_game inicial

---
+++ alien_invasion.py
@@ -0,0 +1,28 @@
+import sys
+import pygame
+
+class AlienInvasion:
+    """
+    Classe que gerencia todo o jogo e seus recursos.
+    """
+
+    def __init__(self):
+        """Inicializa o jogo e cria seus recursos necessários."""
+        pygame.init()
+
+        self.screen = pygame.display.set_mode((800,400))
+        pygame.display.set_caption("Alien Invasion")
+
+    def run_game(self):
+        """Inicia o loop principal do jogo."""
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
+
+            # torna o desenho de tela mais recente visível
+            pygame.display.flip()
+
+if __name__ == '__main__':
+    ai = AlienInvasion()
+    ai.run_game()
