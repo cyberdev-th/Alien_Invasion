e170d56a270a0f4ebb5b84c31e72eed26cee29c9
Thiago Fernandes <devcyber.thiago@gmail.com>
2025-08-08 12:01:04 -0300

    Criando a frota de alienígenas inicial

--- alien_invasion.py
+++ alien_invasion.py
@@ -81,8 +81,15 @@ class AlienInvasion:
     def _create_fleet(self):
         """Cria uma frota de alienígenas."""
         alien = Alien(self)
-        self.aliens.add(alien)
-    
+        alien_width = alien.rect.width
+
+        current_x = alien_width
+        while current_x < (self.settings.screen_width - 2 * alien_width):
+            new_alien = Alien(self)
+            new_alien.x = current_x
+            new_alien.rect.x = current_x
+            self.aliens.add(new_alien)
+            current_x += 2 * alien_width
     
     def _toggle_fullscreen(self, is_fullscreen=True):
         if is_fullscreen:
@@ -109,5 +116,4 @@ class AlienInvasion:
 if __name__ == '__main__':
     ai = AlienInvasion()
     ai.run_game()
-
 
