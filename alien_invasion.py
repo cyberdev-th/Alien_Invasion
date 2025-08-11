9421d14ac769ae8a9a0543a391c0337ee8be0002
Thiago Fernandes <devcyber.thiago@gmail.com>
2025-08-11 13:48:46 -0300

    Verificação de colisão de balas e alienígenas

--- alien_invasion.py
+++ alien_invasion.py
@@ -78,6 +78,9 @@ class AlienInvasion:
         for bullet in self.bullets.copy():
             if bullet.rect.bottom <= 0:
                 self.bullets.remove(bullet)
+        
+        collisions = pygame.sprite.groupcollide(self.bullets,self.aliens,True,True)
+        
 
     def _update_aliens(self):
         """Atualiza as posições dos alienígenas."""
