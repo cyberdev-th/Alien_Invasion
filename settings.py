5aa31fa1e6397c7b3b7646befe0f0d689eca51ed
Thiago Fernandes <devcyber.thiago@gmail.com>
2025-08-11 14:06:07 -0300

    Refatoração do _update_bullets e criação de método específico para tratar de colisões

--- settings.py
+++ settings.py
@@ -19,7 +19,7 @@ class Settings:
         self.ship_speed = 2.5
 
         # configuração das balas(bullets)
-        self.bullet_speed = 6.0
+        self.bullet_speed = 10.0
         self.bullet_width = 3
         self.bullet_height = 15
         self.bullet_color = (60,60,60)
